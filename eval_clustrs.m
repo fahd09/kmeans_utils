088c893fc799bf8edd2eecdc1a38f84a49c740a5
Fahd Alhazmi <fahd09@users.noreply.github.com>
2017-03-16 16:06:55 -0500

    Update eval_clustrs.m

--- eval_clustrs.m
+++ eval_clustrs.m
@@ -1,16 +1,35 @@
 function [sMean,sStd,subj_stab_all]=eval_clustrs(X, krange, dist, runs)
+%eval_clustrs     Evaluate Clusters
+%
+%   S = eval_clustrs(X, krange, dist, runs);
+%   This function calculates the different clustering metrics across range of K's.
+%
+%   Inputs: X,  NxP matrix where N is # of observations (rows) and P
+%   is # of variables.
+%           krange,  1xP vector of the values which metrics are estimated
+%           dist,  a string of the distance function to be used. See 'help kmeans' for different options
+%           rubs,  The nunmber of times metrics are estimated at each k
+%
+%   Output: sMean, sStd are the means and standard deviations of each metric for each K
+%           subj_stab_all, a NxK matrix for the assignments of each observation at each K
+%
+
+
 disp(['Cluster Stability, Benhur method...'])
-S=cluster_stability(X,krange,0.8,runs,dist);
-S(1,:)=[]; sMean=mean(S,2); sStd=std(S,0,2);
+S=cluster_stability(X,krange,0.8,runs,dist);    % random sampling of 2 X 80% of observation at each run
+S(1,:)=[]; sMean=mean(S,2); sStd=std(S,0,2);    % Save means and stds
 
 
-% subject stability
+% calculate observation stability
+% in this metric, we want to know how stable each observation across different values of K
+% i.e. we want to know if some overvations are more likely to switch clusters (less stable)
+% than others.
+
 disp(['calculating subject stability...'])
 IDX=zeros(size(X,1),runs,length(krange));
 for k=krange,
     for m=1:runs,
         IDX(:,m,k) = kmeans(X, k,'Replicates',5,'distance',dist);
-        disp([num2str(m) ' - ' num2str(k)])
     end
 end
 
@@ -19,7 +38,7 @@ subj_stab_all=zeros(size(X,1),max(krange)-1);
 for k=krange,
     cube=zeros(size(X,1),size(X,1),runs);
     for r=1:runs
-        PART1=IDX(:,r,k-1); %IDX(:,r);
+        PART1=IDX(:,r,k-1); 
         C1=zeros(length(PART1),length(PART1));
         for i1=1:length(PART1),
             for j1=1:length(PART1),
